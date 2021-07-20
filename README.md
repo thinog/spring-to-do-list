916d3c12531bbd6117683d5ae0d16a05b438cf24
thinog <thinomar@gmail.com>
2021-07-19 22:57:46 -0300

    Project initial structure

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # spring-to-do-list
+
 To-do list written in Java w/ Spring Boot
 
 Inpired by https://github.com/florinpop17/app-ideas/blob/master/Projects/2-Intermediate/To-Do-App.md
---
+++ core/src/test/java/br/com/todolist/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.todolist.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
